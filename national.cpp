9666d8375c217cc68626a4d56defadeef1c1645c
ngoc6787 <enriaque.omnzg@hotmail.com>
2025-02-25 16:06:03 +0800

    Modify national.cpp

--- national.cpp
+++ national.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Resource read wonder name.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
